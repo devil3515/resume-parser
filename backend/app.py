e9d82757a7ba1bffdfecdc0f9f6603197618884b
devil3515 <Abhi.kumar.3515@gmail.com>
2025-04-19 23:24:44 +0530

    updated app.py for proper hosting

--- backend/app.py
+++ backend/app.py
@@ -88,4 +88,5 @@ def _read_file_from_path(path):
 
 CORS(app)  # Enable CORS for all routes
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
