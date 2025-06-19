45cf7193701b002a1b36648cd3b308090dca7ea4
abhishekkumar-snakescript <abhishekkumar@snakescript.com>
2025-06-19 11:15:01 +0530

    implemented resume parsing in resume-parser-private

--- resume_parser_django/users/urls.py
+++ resume_parser_django/users/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import (
-    RegisterView,
-    CustomTokenObtainPairView,
-    UserProfileView,
-    UpdateUserView
-)
+from .views import CustomTokenObtainPairView, RegisterView, UserProfileView, UpdateUserView, change_password
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -13,4 +8,5 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', UserProfileView.as_view(), name='user_profile'),
     path('profile/update/', UpdateUserView.as_view(), name='update_profile'),
+    path('change-password/', change_password, name='change_password'),
 ] 
